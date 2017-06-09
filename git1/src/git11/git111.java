528f7c25af8a5593c93975ca2dc1b0a16a4e920c
Administrator <Administrator@USER-20170205JN>
2017-06-09 14:25:40 +0800

    去githuber

--- git1/src/git11/git111.java
+++ git1/src/git11/git111.java
@@ -1,9 +1,12 @@
 package git11;
 
+import javax.swing.text.TabableView;
+
 public class git111 {
 	public int sum(int x,int y){return x+y;}
 	public void print(){}
-public static void main(String[] args) {
 	
+public static void main(String[] args) {
+	git111 aTa=new git111(); 
 }
 }
