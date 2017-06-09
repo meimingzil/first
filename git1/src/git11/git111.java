7cd3542a5b804ad82c08a7f740623739a3954396
Administrator <Administrator@USER-20170205JN>
2017-06-09 15:51:27 +0800

    删除

--- git1/src/git11/git111.java
+++ git1/src/git11/git111.java
@@ -4,9 +4,9 @@ import javax.swing.text.TabableView;
 
 public class git111 {
 	public int sum(int x,int y){return x+y;}
-	public void print(){}
+	public static void print(){}
 	
 public static void main(String[] args) {
-	git111 aTa=new git111(); 
+	
 }
 }
